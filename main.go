8b0cb88c117635abc1a5f5bb6c8acc8a57424bb2
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-15 09:12:34 +0700

    feat: add read and setuser methods

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
-package gator
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/phnthnhnm/gator/internal/config"
+)
 
 func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("Failed to read config: %v", err)
+	}
 
+	err = cfg.SetUser("nam")
+	if err != nil {
+		log.Fatalf("Failed to set user: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("Failed to read config: %v", err)
+	}
+
+	fmt.Printf("Config: %+v\n", cfg)
 }
