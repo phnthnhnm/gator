14f46b6a0d000e957038eb39661161053fa881f3
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-15 10:21:10 +0700

    feat: add query/command to delete all users

--- commands.go
+++ commands.go
@@ -82,3 +82,12 @@ func printUser(user database.User) {
 	fmt.Printf(" * ID:      %v\n", user.ID)
 	fmt.Printf(" * Name:    %v\n", user.Name)
 }
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't delete users: %w", err)
+	}
+	fmt.Println("Database reset successfully!")
+	return nil
+}
