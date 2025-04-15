16f1b0f063b0d56ae4ddee2e73003209a2f5b684
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-15 10:33:30 +0700

    feat: add query/command to fetch a single rss feed

--- commands.go
+++ commands.go
@@ -106,3 +106,17 @@ func handlerListUsers(s *state, cmd command) error {
 	}
 	return nil
 }
+
+func handlerAgg(s *state, cmd command) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	feedURL := "https://www.wagslane.dev/index.xml"
+	feed, err := fetchFeed(ctx, feedURL)
+	if err != nil {
+		return fmt.Errorf("failed to fetch feed: %w", err)
+	}
+
+	fmt.Printf("Fetched feed: %+v\n", feed)
+	return nil
+}
